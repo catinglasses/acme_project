bc05643ebf19ab293868967971e3a696451101da
Saveliy Efimov <savva.cat@mail.ru>
2024-04-23 22:04:01 +0300

    add register and change password buttons

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import include, path, reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 import django.contrib.auth
 
 urlpatterns = [
@@ -8,4 +9,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
 ]
